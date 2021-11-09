f85d48606eb515dcbc9f1b6ab03e51578a47cf78
Siwon Lee <58714529+siwon9898@users.noreply.github.com>
2021-11-09 21:26:40 +0900

    yourspage

--- js/colors.js
+++ js/colors.js
@@ -1,19 +1,32 @@
 window.addEventListener("load", function () {
   var colorsWrap = document.querySelector(".colorsWrap")
   var colors_type = []
-  var colors_palette = [
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "navy",
-    "purple",
-    "pink",
-    "red",
+  var colors_palette = []
+  var clr_images = [
+    "clr_red",
+    "clr_brown",
+    "clr_orange",
+    "clr_drkred",
+    "clr_yellow",
+    "clr_white",
+    "clr_blue",
+    "clr_black",
+  ]
+
+  var clr_txts = [
+    "고추장",
+    "궁중",
+    "로제",
+    "기름",
+    "카레",
+    "크림",
+    "식욕감퇴",
+    "짜장",
   ]
 
   var clr_stopBtn = document.querySelector(".clr_stopBtn")
+  var clr_centerimg = document.querySelector(".clr_centerimg")
+  var clr_centertxt = document.querySelector(".clr_centertxt")
 
   clr_stopBtn.addEventListener("click", function () {
     colorsWrap.classList.toggle("active")
@@ -36,7 +49,7 @@ window.addEventListener("load", function () {
   }
   clr_dotsAdder()
   var clr_dots = document.querySelectorAll(".colorsWrap div")
-  console.log(clr_dots)
+  // console.log(clr_dots)
   function clr_classAdder() {
     for (var i = 0; i < colors_type.length; i++) {
       clr_dots[i].classList.add(colors_type[i])
@@ -71,16 +84,37 @@ window.addEventListener("load", function () {
     one.addEventListener("mouseover", function () {
       colorsWrap.classList.add("active")
       one.classList.add("active")
-      one.style.background = colors_palette[index]
       clr_stopBtn.value = "START"
+      console.log(clr_centertxt)
+      document.getElementsByTagName("h3")[0].innerHTML =
+        clr_txts[index] + " 떡볶이"
+      clr_centertxt.classList.add("active")
+      clr_centerimg.classList.add("active")
+      clr_centerimg.src = `/majestic_tteokbokki/image/${clr_images[index]}.png`
+
+      const colorThief = new ColorThief()
+
+      clr_centerimg.onload = function () {
+        colors_palette = colorThief.getPalette(clr_centerimg)
+        for (var i = 0; i < clr_dots.length; i++) {
+          clr_dots[i].style.transition = "1s ease-in-out"
+          clr_dots[i].style.background = "rgba(" + colors_palette[i] + ")"
+        }
+      }
     })
   )
+
   clr_dots.forEach((one) =>
     one.addEventListener("mouseout", function () {
       colorsWrap.classList.remove("active")
       one.classList.remove("active")
       one.style.background = "white"
+      for (var i = 0; i < clr_dots.length; i++) {
+        clr_dots[i].style.background = "white"
+      }
       clr_stopBtn.value = "STOP"
+      clr_centerimg.classList.remove("active")
+      clr_centertxt.classList.remove("active")
     })
   )
 })
